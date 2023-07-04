361341365a85a3a474e7c8c156767d7c7a41c410
Crowdbotics <engineering@crowdbotics.com>
2023-07-04 14:39:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-sea-dev-97405",
+  url: "https://withered-sea-dev-97405.botics.co",
+  api: "https://withered-sea-dev-97405.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
